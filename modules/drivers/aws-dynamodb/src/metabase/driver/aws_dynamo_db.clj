dbca794b694a63a92fad54f0a20593f0a01723bd
Cameron Barre <cjbarre@gmail.com>
2019-07-17 21:01:37 -0700

    First cut of can-connect?

--- modules/drivers/aws-dynamodb/src/metabase/driver/aws_dynamo_db.clj
+++ modules/drivers/aws-dynamodb/src/metabase/driver/aws_dynamo_db.clj
@@ -1,1 +1,30 @@
-(ns metabase.driver.aws-dynamo-db)
+(ns metabase.driver.aws-dynamo-db
+  (:require [metabase.driver :as driver]
+            [cognitect.aws.client.api :as aws]
+            [cognitect.aws.credentials :as aws-creds]
+            [cognitect.aws.region :as aws-region]
+            [cognitect.anomalies :as anom]
+            [clojure.spec.alpha :as s]))
+
+(driver/register! :aws-dynamo-db)
+
+(defn get-client 
+  [{:keys [api region access-key-id secret-access-key] :as args}]
+  (aws/client {:api api
+               :region region
+               :credentials-provider
+               (aws-creds/basic-credentials-provider
+                (select-keys args [:access-key-id :secret-access-key]))}))
+
+(defn can-connect?
+  [{:keys [api region table-name access-key-id secret-access-key] :as details}]
+  (if (s/valid? ::anom/anomaly 
+                (aws/invoke (get-client details)
+                            {:op :DescribeTable
+                             :request {:TableName table-name}}))
+    false
+    true))
+
+(defmethod driver/can-connect? :aws-dynamo-db
+  [_ details]
+  (can-connect? details))
